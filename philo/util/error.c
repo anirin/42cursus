52e15e22f45ffe415d550ab6a1a2dd558f98210c
anirin <atu8823atu@gmail.com>
2024-06-17 16:36:40 +0900

    fix error message

--- philo/util/error.c
+++ philo/util/error.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atsu <atsu@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: atokamot <atokamot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 14:59:36 by atokamot          #+#    #+#             */
-/*   Updated: 2024/06/17 13:57:15 by atsu             ###   ########.fr       */
+/*   Updated: 2024/06/17 16:36:03 by atokamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,18 @@ int	check_error(int argc, char *argv[])
 	}
 	if (ft_atoi(argv[1]) < 1)
 	{
-		printf("Error: Number of philosophers must be greater than 0\n");
+		printf("Error: Wrong number of philosophers\n");
 		return (ERROR);
 	}
 	if (ft_atoi(argv[4]) < 1 || ft_atoi(argv[3]) < 1 || ft_atoi(argv[2]) < 1)
 	{
-		printf("Error: Time to die, eat, and sleep must be greater than 0\n");
+		printf("Error: Wrong number of Time to die, eat, and sleep\n");
 		return (ERROR);
 	}
 	if (argc == 6 && ft_atoi(argv[5]) < 1)
 	{
-		printf("Error: Number of times \
-		each philosopher must eat must be greater than 0\n");
+		printf("Error: Wrong number of times \
+		each philosopher must eat\n");
 		return (ERROR);
 	}
 	return (OK);
