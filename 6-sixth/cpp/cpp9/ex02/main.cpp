809400d6637affa64094a994ee237b863d00e4c6
anirin <atu8823atu@gmail.com>
2025-02-13 16:45:15 +0900

    sort 重複許可完了

--- 6-sixth/cpp/cpp9/ex02/main.cpp
+++ 6-sixth/cpp/cpp9/ex02/main.cpp
@@ -2,22 +2,20 @@
 
 int d_jacob[] = {0,2,2,6,10,22,42,86,170,342,682,1366,2730,5462,10922,21846,43690,87382,174762,349526,699050,1398102,2796202,5592406,11184810};
 int node_num[] = {0,1,3,7,15,31,63,127,255,511,1023,2047,4095,8191,16383,32767,65535,131071,262143,524287,1048575,2097151,4194303,8388607,16777215};
+// node_num の index がそのまま最大の比較回数となる
 
-void insertPart(std::vector<Chain*> &array, Chain *chain, int index)
+int comp_count = 0;
+int max_comp_count = 0;
+
+void insertPart(std::vector<Chain*> &array, Chain *chain, int index) // copyによって省力化を図ることはできる
 {
+	std::cout << "Insert " << chain->getNum() << " at " << index << std::endl;
+
 	std::vector<Chain*> new_array;
 
-	// std::cout << "Insert: " << chain->getNum() << " at " << index << std::endl;
-
-	if (index < 0) {
-		throw std::runtime_error("[Insert Part] Index is less than 0");
-	}
-
-	// 重複の場合は先頭に入れる
 	for (int i = 0; i < array.size(); i++) {
-		if (i == index) {
+		if (i == index)
 			new_array.push_back(chain);
-		}
 		new_array.push_back(array[i]);
 	}
 	if (index == array.size()) {
@@ -28,102 +26,106 @@ void insertPart(std::vector<Chain*> &array, Chain *chain, int index)
 	array = new_array;
 }
 
-int binarySearch(std::vector<Chain*> array, int start_index, int end_index, int size, Chain *value)
+int binarySearch(std::vector<Chain*> array, int start_index, int end_index, Chain *node)
 {
+	int size = end_index - start_index + 1;
 	int center_index;
 	center_index = size / 2 + start_index;
 
+	comp_count++;
 
-	if (array[center_index]->getNum() == value->getNum() ) // 重複の場合は先頭に入れる、即座に場所を確定させるため災厄のケースでも処理は重たくならない
-	{
-		return center_index;
-	}
-	else if (array[center_index]->getNum() < value->getNum())
+	if (*array[center_index] < *node)
 	{
 		if (center_index == end_index)
-		{
 			return center_index + 1;
-		}
-		return binarySearch(array, center_index + 1, end_index, end_index - center_index, value);
+		return binarySearch(array, center_index + 1, end_index, node);
 	}
-	else // array[center_index]->getNum() > value->getNum()
+	else
 	{
 		if (center_index == start_index)
-		{
 			return center_index;
-		}
-		return binarySearch(array, start_index, center_index - 1, center_index - start_index, value);
+		return binarySearch(array, start_index, center_index - 1, node);
 	}
 }
 
-// あくまでも２つのソート済み配列をマージする関数
-void mergePart(std::vector<Chain*> &array, std::vector<Layer> layers, int layer_index)
+void getSmallArrays(std::vector<Chain*> array, std::vector<Chain*> &small_chains, std::vector<Layer> layers, int layer_index)
 {
-	// std::cout << "[big array] : ";
-	// printChainArray(array);
-
-	if (layer_index == 0)
-		return;
-
-	std::vector<Chain*> small_chains;
 	for (std::vector<Chain*>::iterator it = array.begin(); it != array.end(); ++it)
 	{
 		Chain* pair;
 		pair = (*it)->getPair(layer_index);
 		small_chains.push_back(pair);
 	}
-
-	int init_insert_size = small_chains.size();
-	if (layers[layer_index - 1].hasRest) // 挿入タイミングが間違っている && -1の場合は最初の挿入
+	if (layers[layer_index - 1].hasRest)
 	{
-		init_insert_size++;
 		small_chains.push_back(layers[layer_index - 1].rest);
 	}
+}
 
-	// std::cout << "[small array] : ";
-	// printChainArray(small_chains);
+int getEnd(int count, std::vector<Chain*> array)
+{
+	int end;
 
-	insertPart(array, small_chains[0], 0);
-	init_insert_size--;
+	if (node_num[count + 2] < array.size()) // node_num[1] = 3, 7 ....
+		end = node_num[count + 2] - 1;
+	else
+		end = array.size() - 1;
 
+	return end;
+}
+
+int getInsertSize(int s_size, int count)
+{
+	int insert_size;
+
+	if (d_jacob[count + 1] < s_size) // d_jacob[1] = 2, 2, 6 ....
+		insert_size = d_jacob[count + 1];
+	else
+		insert_size = s_size;
+	
+	return insert_size;
+}
+
+// あくまでも２つのソート済み配列をマージする関数
+void mergePart(std::vector<Chain*> &array, std::vector<Layer> layers, int layer_index)
+{
+	if (layer_index == 0)
+		return;
+
+	std::vector<Chain*> small_array;
+	int s_size;
 	int count = 0;
 	int base = 0;
 
+	getSmallArrays(array, small_array, layers, layer_index);
+	s_size = small_array.size();
+
+	std::cout << "[big array] : "; printChainArray(array);
+	std::cout << "[small array] : "; printChainArray(small_array);
+
+	insertPart(array, small_array[0], 0);
+	s_size--;
+
 	for (;;)
 	{
-		if (init_insert_size == 0)
+		if (s_size == 0)
 			break;
 
-		int end = 0;
-		int insert_size = 0;
+		int end = getEnd(count, array);
+		int insert_size = getInsertSize(s_size, count);
 
-		if (node_num[count + 4] < array.size())
-			end = node_num[count + 4] - 1;
-		else
-			end = array.size() - 1;
+		max_comp_count += insert_size * (count + 2);
 
-		if (d_jacob[count + 1] < init_insert_size)
-			insert_size = d_jacob[count + 1];
-		else
-			insert_size = init_insert_size;
-
-		// std::cout << "new insert size: " << insert_size << std::endl;
+		std::cout << " ======== End: " << end << ", Insert Size: " << insert_size << " ========" << std::endl;
 		for (int i = 0; i < insert_size; i++)
 		{
-			if (array.size() == 0)
-				break;
+			Chain* node = small_array[insert_size - i + base];
+			int insert_index = binarySearch(array, 0, end, node);
 
-			Chain *value;
-			int target;
-
-			value = small_chains[insert_size - i + base];
-
-			// printChainArray(array);
-			target = binarySearch(array, 0, end, end + 1, value);
-			insertPart(array, value, target);
-
-			init_insert_size--;
+			insertPart(array, node, insert_index);
+			s_size--;
 		}
+
 		base += insert_size;
 		count++;
 	}
@@ -134,9 +136,7 @@ Chain* getTop(std::vector<Layer> layers)
 	for (int i = 0; i < layers.size(); i++)
 	{
 		if (layers[i].hasTop)
-		{
 			return layers[i].top;
-		}
 	}
 	return NULL;
 }
@@ -150,18 +150,14 @@ void sort(std::vector<Chain*> &array, std::vector<Layer> layers)
 
 	for (int i = layer_size - 1; i >= 0; --i)
 	{
-		// std::cout << "===== Layer Index: " << i << " =====" << std::endl;
+		std::cout << "===== Layer Index: " << i << " =====" << std::endl;
 		mergePart(array, layers, i);
 	}
 }
 
 void makeLayer(std::vector<Chain*> &array, std::vector<Layer> &layers)
 {
-	// for (int i = 0; i < array.size(); i++)
-	// {
-	// 	std::cout << array[i]->getNum() << " ";
-	// }
-	// std::cout << std::endl << std::endl;
+	printChainArray(array);
 
 	int i = 0;
 	int size = array.size();
@@ -215,7 +211,7 @@ std::vector<Chain*> initChain(int size, int range)
 
 	for (int i = 0; i < size; i++)
 	{
-		Chain *chain = new Chain(rand() % range, nullptr);
+		Chain *chain = new Chain(rand() % range, NULL);
 		chains.push_back(chain);
 	}
 	return chains;
@@ -233,29 +229,33 @@ bool isSorted(std::vector<Chain*> array)
 
 int main()
 {
-	int size = 50;
+	int size = 1;
+	int length = 600;
+	int range = 1000000;
 
 	for (int i = 0; i < size; i++)
 	{
 		std::vector<Chain*> chains;
 		std::vector<Layer> layers;
 
-		chains = initChain(1000 - i, 10000);
+		chains = initChain(length - i, range);
 
-		// printChainArray(chains);
+		printChainArray(chains);
 
 		makeLayer(chains, layers);
 
-		// std::cout << "======================" << std::endl << std::endl;
-		// printLayer(layers);
+		std::cout << "======================" << std::endl << std::endl;
+		printLayer(layers);
 
-		// std::cout << "======================" << std::endl << std::endl;
-		// printChain(getTop(layers), 0);
+		std::cout << "======================" << std::endl << std::endl;
+		printChain(getTop(layers), 0);
 
-		// std::cout << "======================" << std::endl << std::endl;
+		std::cout << "======================" << std::endl << std::endl;
 		std::vector<Chain*> sorted_array;
 		sort(sorted_array, layers);
-		// printChainArray(sorted_array);
+		printChainArray(sorted_array);
+		
+		std::cout << "max comp count: " << max_comp_count << ", comp count: " << comp_count << std::endl;
 
 		if (!isSorted(sorted_array))
 		{
